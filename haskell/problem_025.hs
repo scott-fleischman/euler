8ddb4fd923373bc270a7ec40191b7f7ec0099dc7
Scott Fleischman <scott.fleischman@logos.com>
2014-06-08 21:39:35 -0700

    Use spaces instead of tabs.

--- haskell/problem_025.hs
+++ haskell/problem_025.hs
@@ -1,7 +1,7 @@
 fibs = 1 : 1 : fib 1 1
-	where
-		fib n1 n2 = n3 : fib n2 n3
-			where n3 = n1 + n2
+  where
+    fib n1 n2 = n3 : fib n2 n3
+      where n3 = n1 + n2
 
 indexedFibs = zip [1..] fibs
 

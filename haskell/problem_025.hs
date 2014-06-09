e5822ecd996b67de2018a7000722ba6c6a35932a
Scott Fleischman <scott.fleischman@logos.com>
2014-06-08 21:38:55 -0700

    Add problem 25

---
+++ haskell/problem_025.hs
@@ -0,0 +1,8 @@
+fibs = 1 : 1 : fib 1 1
+	where
+		fib n1 n2 = n3 : fib n2 n3
+			where n3 = n1 + n2
+
+indexedFibs = zip [1..] fibs
+
+main = putStrLn . show . fst . head . dropWhile (\(i,x) -> x < 10^999) $ indexedFibs

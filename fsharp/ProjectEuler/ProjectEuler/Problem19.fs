def0d6785d34b6118ca5b4a8dd7c3aa4eed02ae8
Scott Fleischman <scott.fleischman@logos.com>
2016-02-10 09:42:45 -0800

    Experiment with different layout of getNextDate function.

--- fsharp/ProjectEuler/ProjectEuler/Problem19.fs
+++ fsharp/ProjectEuler/ProjectEuler/Problem19.fs
@@ -10,7 +10,6 @@ type Date =
     ; year : Year
     }
 type LeapYear = IsLeapYear | NotLeapYear
-type Carry = HasCarry | NoCarry
 
 let isLeapYear y = if y % 4 = 0 && (y % 100 <> 0 || y % 400 = 0) then IsLeapYear else NotLeapYear
 
@@ -49,17 +48,25 @@ let getDaysInMonth leap =
     | _ -> 31
 
 let getNextDate date =
-    match date with
-    | { dayOfMonth = DayOfMonth dayNumber; month = month; year = Year yearNumber } ->
-        let leap = isLeapYear yearNumber
-        let lastDay = getDaysInMonth leap month
-        if dayNumber <> lastDay
-        then { date with dayOfMonth = DayOfMonth (dayNumber + 1) }
+    let { dayOfMonth = DayOfMonth dayNumber; month = month; year = Year yearNumber } = date
+
+    let startNextMonth () =
+        let firstDay = DayOfMonth 1
+        let nextMonth = getNextMonth month
+        let nextDate = { date with dayOfMonth = firstDay; month = nextMonth }
+
+        if nextMonth <> January
+        then nextDate
         else
-            let nextMonth = getNextMonth month
-            if nextMonth <> January
-            then { date with dayOfMonth = DayOfMonth 1; month = nextMonth }
-            else { dayOfMonth = DayOfMonth 1; month = nextMonth; year = Year (yearNumber + 1) }
+            let nextYear = Year (yearNumber + 1)
+            { nextDate with year = nextYear }
+
+    let leap = isLeapYear yearNumber
+    let lastDay = getDaysInMonth leap month
+
+    if dayNumber <> lastDay
+    then { date with dayOfMonth = DayOfMonth (dayNumber + 1) }
+    else startNextMonth ()
 
 let nextPair (dayOfWeek, date) = (getNextDayOfWeek dayOfWeek, getNextDate date)
 
